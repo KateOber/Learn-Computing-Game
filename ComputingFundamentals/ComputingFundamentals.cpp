13f663029dcd58cae97f5eb7f4e692ba022db829
Kate <obertase@student.douglascollege.ca>
2021-10-20 06:49:29 -0700

    Added first screen

--- ComputingFundamentals/ComputingFundamentals.cpp
+++ ComputingFundamentals/ComputingFundamentals.cpp
@@ -1,20 +1,28 @@
 // ComputingFundamentals.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <SFML/Graphics.hpp>
 
-#include <iostream>
+using namespace sf;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    VideoMode vm (1920, 1080);
+    RenderWindow window(vm, "CSIS4495 Project", Style::Fullscreen);
+
+    Texture firstScreenTexture;
+    Sprite firstScreenSprite;
+    firstScreenTexture.loadFromFile("Assets/Graphics/first-screen.png");
+    firstScreenSprite.setTexture(firstScreenTexture);
+    firstScreenSprite.setPosition(0, 0);
+
+    while (window.isOpen()) {
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+            window.close();
+        }
+        window.clear();
+        window.draw(firstScreenSprite);
+        window.display();
+    }
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
